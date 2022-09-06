317f3225fa240ced3d129a520c55c9af6b80e037
jenn-codes <93089897+jenn-codes@users.noreply.github.com>
2022-09-05 20:34:27 -0400

    style: move like button to the left of card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,9 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import ArrowCircleUpOutlinedIcon from '@mui/icons-material/ArrowCircleUpOutlined';
+import ArrowCircleDownOutlinedIcon from '@mui/icons-material/ArrowCircleDownOutlined';
 import { Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
 
 
 const PostCard = ({ board, 
@@ -35,39 +37,48 @@ const PostCard = ({ board,
 
     return (
         <div className='card'>
-            <Card sx={{ width: 600 }}>
-                <CardContent>
-                    <Typography variant="caption" align="right" component="div">
-                        Posted under <Link to={`/board/${board}/`}>{board}</Link>
-                    </Typography>
-                    <Typography variant="caption" align="left" component="div">
-                        Posted on {date} by <Link to={`/user/${user}/`}>{user}</Link>
-                    </Typography>
-                    <Link to={`/post/${id}/`} style={{ textDecoration: 'none' }}>
-                    <Typography variant="h4" align="left" color="black" component="div">
-                        {title}
-                    </Typography>
-
-                    <CardMedia
-                        component="img"
-                        height="200"
-                        image={image}
-                        alt={board}
-                    />
-                    </Link>
-                    <Typography variant="body2" align="left" color="text.secondary">
-                        {body}
-                    </Typography>
-                    <Typography variant="caption" align="left" component="div">
-                        <Link to="{url}">{url}</Link>
-                    </Typography>
-                    <CardActions>
-                        <Button size="small" onClick={() => handleLike(id)}><ArrowCircleUpOutlinedIcon /> {likes} likes </Button>
+            <Card sx={{ width: 600 }}>               
+                <Grid container  >
+                    <Grid item xs={1} direction="column" container justifySelf="center">
+                        <Button align="left" size="small" onClick={() => handleLike(id)}><ArrowCircleUpOutlinedIcon /></Button>
+                        {likes} 
+                        <Button align="left" size="small"><ArrowCircleDownOutlinedIcon /></Button>
+                    </Grid>
+                    <Grid item xs={11}>
+                    <CardContent>
+                        <Typography variant="caption" align="right" component="div">
+                            Posted under <Link to={`/board/${board}/`}>{board}</Link>
+                        </Typography>
+                        <Typography variant="caption" align="left" component="div">
+                            Posted on {date} by <Link to={`/user/${user}/`}>{user}</Link>
+                        </Typography>
                         <Link to={`/post/${id}/`} style={{ textDecoration: 'none' }}>
-                        <Button size="small"><ChatBubbleOutlineIcon />{comments} comments</Button>
+                        <Typography variant="h4" align="left" color="black" component="div">
+                            {title}
+                        </Typography>
+
+                        <CardMedia
+                            component="img"
+                            height="200"
+                            image={image}
+                            alt={board}
+                        />
                         </Link>
-                    </CardActions>
-                </CardContent>
+                        <Typography variant="body2" align="left" color="text.secondary">
+                            {body}
+                        </Typography>
+                        <Typography variant="caption" align="left" component="div">
+                            <Link to="{url}">{url}</Link>
+                        </Typography>
+                        <CardActions>
+                            
+                            <Link to={`/post/${id}/`} style={{ textDecoration: 'none' }}>
+                                <Button size="small"><ChatBubbleOutlineIcon />{comments} comments</Button>
+                            </Link>
+                        </CardActions>
+                    </CardContent>
+                    </Grid>
+                </Grid>
             </Card>
         </div>
     )
