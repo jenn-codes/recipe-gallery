6ed4b3597e52d4b8da9edc6bcb41cbdba0402842
jenn-codes <93089897+jenn-codes@users.noreply.github.com>
2022-09-17 18:42:07 -0400

    feat: allow user to log out from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,12 +12,13 @@ import MenuItem from '@mui/material/MenuItem';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { getAuth } from "firebase/auth";
 
-
-const settings = ['Profile', 'Logout'];
 
 const Header = () => {
   const [anchorElUser, setAnchorElUser] = useState(null);
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -26,21 +27,52 @@ const Header = () => {
   };
 
   const searchRef = useRef('');
+
   const navigate = useNavigate();
   const handleSearch = () => {
     console.log(searchRef.current.value);
     navigate(`./search/${searchRef.current.value}`)
   }
 
+  const logout = () => {
+    const auth = getAuth();
+    auth.signOut();
+    console.log('User signed out!');
+  }
 
-  const allSettings = settings.map((setting) => (
-    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-      <Typography textAlign="center">{setting}</Typography>
-    </MenuItem>
-  ))
+  const allSettings = (
+    <div>
+      <Tooltip title="Open settings">
+      <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }} style={{align: "right"}} >
+        <Avatar  alt="Jennifer" src="/static/images/avatar/2.jpg" />
+      </IconButton>
+      </Tooltip>
+      <Menu
+      sx={{ mt: '45px' }}
+      id="menu-appbar"
+      anchorEl={anchorElUser}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={Boolean(anchorElUser)}
+      onClose={handleCloseUserMenu}
+      >
+      <MenuItem onClick={handleCloseUserMenu} style={{display: 'flex', flexDirection: 'column'}}>
+              <Typography textAlign="center" onClick={() => navigate('/user/100')} >Profile</Typography>
+              <Typography textAlign="center"  onClick={logout}>Logout</Typography>
+            </MenuItem>
+      </Menu>
+
+    </div>
+  )
   
 
-  
 
   return (
     <AppBar className="header" position="static" style={{backgroundColor: 'white'}} >
@@ -73,32 +105,12 @@ const Header = () => {
             </IconButton>
           </Box>
 
-
-          <Box sx={{ flexGrow: 0, justifySelf: "flex-end" }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }} style={{align: "right"}} >
-                <Avatar  alt="Jennifer" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {allSettings}
-            </Menu>
+          <Box sx={{ flexGrow: 0, justifySelf: "flex-end", display: "flex", gap: 2}}>
+            <Button size="small" variant="contained" sx={{backgroundColor: 'white', color: 'darkorange'}} onClick={() => navigate('./login')}>Login</Button>
+            <Button size="small" variant="contained" sx={{backgroundColor: 'darkorange'}} onClick={() => navigate('./register')}>Register</Button>
+            {allSettings}
           </Box>
+
         </Toolbar>
       </Container>
     </AppBar>
--- src/components/User.js
+++ src/components/User.js
@@ -5,16 +5,41 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import { Grid } from '@mui/material';
+
 
 const User = () => {
-    
+
+
+    const navigate = useNavigate();
     const params = useParams();
     const paramId = params.id;
+    console.log(paramId);
     const auth = getAuth();
-    const [uid, setUid] = useState(null)
-    const [email, setEmail] = useState(null)
-    const [displayName, setDisplayName] = useState(null)
+    const [uid, setUid] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [displayName, setDisplayName] = useState(null);
+    const [newUrl, setNewUrl] = useState('');
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
 
+    console.log(uid)
+
+
+    useEffect(() => {       
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setEmail(user.email);
+                setUid(user.uid);
+                setDisplayName(user.displayName);
+                setNewUrl(`/user/${user.uid}`); 
+                setUserLoggedIn(true);
+                navigate(newUrl)
+                console.log(newUrl)
+            } 
+        })   
+
+    }, [auth, navigate, newUrl])
     
     const updateName = () => {
         updateProfile(auth.currentUser, {
@@ -24,43 +49,41 @@ const User = () => {
     }
 
 
-
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setEmail(user.email);
-                setUid(user.uid);
-                setDisplayName(user.displayName);
-            } 
-        })   
-
-    }, [auth])
-
-
     return ( 
         <div>
-            <span>User</span>
+            {userLoggedIn ? 
             
             <Box
                 component="form"
                 className="add-post"
                 sx={{
                     backgroundColor: 'white', borderRadius: 2, margin: 'auto',  
-                    '& > :not(style)': { m: 1, width: '25ch' },
+                    '& > :not(style)': { m: 1, width: '40' },
                 }}
                 noValidate
                 autoComplete="off"
                 >
                 <Typography variant="h4" sx={{color: 'gray', textAlign: 'center', margin: 'auto'}} >
-                    Update your display name.
+                    User Data
                 </Typography>
+
+                <Grid container spacing={2} style={{width: '80%', margin: 'auto'}}>
+                    <Grid item xs={10} style={{padding: 0}}>
+                        <TextField id="displayName" label="Enter a display name." onChange={(e) => setDisplayName(e.target.value)} variant="standard" style = {{width: '90%', padding: 0}} />
+                    </Grid>
+                    <Grid item xs={2} style={{padding: 0, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end'}}>
+                        <Button size="small" variant="outlined" sx={{backgroundColor: 'white', padding: 0, alignSelf: 'center', justifySelf: 'center'}} onClick={updateName}  style={{padding: 0,  verticalAlign: 'middle'}}>Update</Button>
+                    </Grid>
+                </Grid>
                 
-                <TextField id="displayName" label="Enter a display name." onChange={(e) => setDisplayName(e.target.value)} variant="standard" style = {{width: '80%'}}/>
-                <Button size="small" variant="contained" sx={{backgroundColor: 'darkorange'}} onClick={updateName}>Update username.</Button>
+                <Typography variant="p" align="center" style={{width: '80%'}}>User Email: {email}</Typography>
+                <Typography variant="p" align="center"  style={{width: '80%'}}>Display Name: {displayName}</Typography>
+                <Typography variant="p" align="left" >Posts Submitted by User:</Typography>
+
             </Box>
-            <span>{uid}</span>
-            <span>{email}</span>
-            <span>{displayName}</span>
+
+            : <span>You are not logged in. Log in to continue.</span>}
+
 
 
         </div>
